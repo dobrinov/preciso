271744c9cbdc8138460adaa7c82ffa549d1736a2
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2026-06-08 15:57:57 +0300

    Add storefront variant switcher and from-price display

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,7 @@ class ProductsController < ApplicationController
     @product = Product.find_by(id: params[:id])
     return render "shared/not_found", status: :not_found unless @product
 
+    @variants = @product.variants.includes(:variant_attribute_values, images_attachments: :blob)
     @category = @product.category
     @related = @category ? @category.products.where.not(id: @product.id).order(:id).limit(4) : []
     track("product/#{@product.id}", @product.name, piece: true, name: @product.name)
